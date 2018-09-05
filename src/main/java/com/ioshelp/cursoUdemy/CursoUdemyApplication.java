896e72ed88784bb43285d9fb04581678f967513f
Alex Trindade <trindadealex@gmail.com>
2018-09-05 17:45:17 -0300

    Estado e cidade

--- src/main/java/com/ioshelp/cursoUdemy/CursoUdemyApplication.java
+++ src/main/java/com/ioshelp/cursoUdemy/CursoUdemyApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ioshelp.cursoUdemy.domain.Categoria;
+import com.ioshelp.cursoUdemy.domain.Cidade;
+import com.ioshelp.cursoUdemy.domain.Estado;
 import com.ioshelp.cursoUdemy.domain.Produto;
 import com.ioshelp.cursoUdemy.repositories.CategoriaRepository;
+import com.ioshelp.cursoUdemy.repositories.CidadeRepository;
+import com.ioshelp.cursoUdemy.repositories.EstadoRepository;
 import com.ioshelp.cursoUdemy.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursoUdemyApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUdemyApplication.class, args);
@@ -40,8 +50,22 @@ public class CursoUdemyApplication implements CommandLineRunner{
  		p1.getCategorias().addAll(Arrays.asList(cat1));
  		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
  		p3.getCategorias().addAll(Arrays.asList(cat1));
-
+ 		
  		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
  		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+ 		
+ 		Estado est1 = new Estado(null, "Minas Gerais");
+ 		Estado est2 = new Estado(null, "São Paulo");
+ 		
+ 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+ 		Cidade c2 = new Cidade(null, "São Paulo", est2);
+ 		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+ 		est1.getCidades().addAll(Arrays.asList(c1));
+ 		est2.getCidades().addAll(Arrays.asList(c2, c3));
+ 		
+ 		estadoRepository.saveAll(Arrays.asList(est1, est2));
+ 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+ 		
 	}
 }
---
+++ src/main/java/com/ioshelp/cursoUdemy/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.ioshelp.cursoUdemy.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ioshelp.cursoUdemy.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
